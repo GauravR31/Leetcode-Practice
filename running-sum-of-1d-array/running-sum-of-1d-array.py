e9c5fa7bebc7ea67679f7fd8b439cbcb1ed90de5
Gaurav Ramnani <gauravramnani007@gmail.com>
2021-12-23 13:34:47 -0800

    Delete running-sum-of-1d-array directory

--- running-sum-of-1d-array/running-sum-of-1d-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        runningSum = []
-        
-        for i in range(0, len(nums)):
-            runningSum.append(sum(nums[:i+1]))
-            
-        return runningSum
