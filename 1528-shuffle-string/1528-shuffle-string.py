cff02c996451d2b7f45573007177924365a29941
Gaurav Ramnani <gauravramnani007@gmail.com>
2022-01-06 14:54:20 -0800

    Time: 97 ms (9.35%), Space: 14.4 MB (21.28%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,11 +1,11 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        resList = list(s).copy()
+        resList = [""]*len(s)
         sIter = 0
         
         for i in indices:
             resList[i] = s[sIter]
             sIter += 1
             
-        return ''.join(resList)
+        return "".join(resList)
         
