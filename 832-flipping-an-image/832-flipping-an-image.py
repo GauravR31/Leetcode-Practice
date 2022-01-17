6a85a2318fc65f14f939c207ade4c3c22c7ca87d
Gaurav Ramnani <gauravramnani007@gmail.com>
2022-01-16 17:00:24 -0800

    Time: 104 ms (5.27%), Space: 14.3 MB (24.16%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -3,7 +3,6 @@ class Solution:
         for r in range(len(image)):
             image[r] = image[r][::-1]
             
-        for r in range(len(image)):
             for c in range(len(image[r])):
                 if image[r][c] == 0:
                     image[r][c] = 1
