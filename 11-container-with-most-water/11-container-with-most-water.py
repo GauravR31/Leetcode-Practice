236e17706ff3306145324e6ecba528bc3d0830f0
Gaurav Ramnani <gauravramnani007@gmail.com>
2022-04-05 22:42:35 -0400

    Time: 1014 ms (43.35%), Space: 27.4 MB (58.35%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -9,14 +9,11 @@ class Solution:
         while l < r:
             vol = (r-l) * min(height[l], height[r])
             max_vol = max(vol, max_vol)
-            # print(l, r, height[l], height[r], vol, max_vol)
             
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        
-        
 #         for i in range(l-1):
 #             for j in range(i+1, l):
 #                 vol = (j-i) * min(height[i], height[j])
